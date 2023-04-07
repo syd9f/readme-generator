6f735290cf81a05d549866ec1615b923d2aa1315
sydnief <sydniefarrell@gmail.com>
2023-04-06 23:24:40 -0400

    cleaned code

--- assets/index.js
+++ assets/index.js
@@ -15,47 +15,53 @@ inquirer
     },
     {
       type: 'input',
-      message: 'List Table of Contents',
-      name: 'toc',
-    },
-    {
-      type: 'input',
       message: 'Enter Installation requirements:',
       name: 'install',
     },
     {
-        type: 'input',
-        message: 'Enter Usage information:',
-        name: 'usage',
-      },
-    {
       type: 'input',
+      message: 'Enter Usage information:',
+      name: 'usage',
+    },
+    {
+      type: 'list',
       message: 'Select a License from the following options:',
       name: 'license',
+      choices: ['one','two','three'],
     },
     {
-        type: 'input',
-        message: 'Enter Contribution Guidelines;',
-        name: 'contibuting',
+      type: 'input',
+      message: 'Enter Contribution Guidelines;',
+      name: 'contibuting',
     },
     {
-        type: 'input',
-        message: 'Enter Test Instructions:',
-        name: 'tests',
+      type: 'input',
+      message: 'Enter Test Instructions:',
+      name: 'tests',
     },
     {
-        type: 'input',
-        message: 'Enter Questions:',
-        name: 'questions',
+      type: 'input',
+      message: 'Enter GitHub username:',
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'Enter email:',
+      name: 'email',
     },
 
   ])
-  .then((response) =>
-    fs.appendFile('README.md',JSON.stringify(response, null, '\t'), (err) => {
+  .then((response) => {
+    // TODO: append to readme
+    const title = '## ' + response.title.toUpperCase();
+    // TODO: append each to readme as title of each section
+    const headers = ['Description','Table of Contents','Installation','Usage','License','Contributing','Tests','Questions'];
+
+    fs.writeFile('README.md',JSON.stringify(response, null, '\t'), (err) => {
         if(err) {
             console.log('There was an error: ' + err);
         } else {
             console.log('got it');
         }
     })
-  );
+  });
